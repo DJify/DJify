16ecd6fd990f6117cb93bec52c835d818a64d31f
Adib Khan <adib.khan@gmail.com>
2019-11-17 06:50:24 -0500

    Get Playlists

--- frontend/src/room/index.js
+++ frontend/src/room/index.js
@@ -19,18 +19,15 @@ const fakeSong = {
   albumImg: "http://i.imgur.com/mjqJhdD.jpg"
 };
 
-const fakeQueue = [
+const fakePlaylists = [
   {
-    title: "Barboy",
-    artist: "The Weekend",
+    name: "Barboy",
   },
   {
-    title: "Carboy",
-    artist: "The Weekend",
+    name: "Carboy",
   },
   {
-    title: "Scarboy",
-    artist: "The Weekend",
+    name: "Scarboy",
   },
 ];
 
@@ -40,68 +37,112 @@ class Room extends Component {
     super(props);
     this.state = {
       isDj: true,
-      amountAhead: 2,
-      items: fakeQueue,
-      percent: -23,
+      selected: 0,
+      //amountAhead: 2,
+      items: fakePlaylists,
+      changedValues: false,
+      //percent: -23,
     };
-    this.handleOutletOrder = this.handleOutletOrder.bind(this);
+    this._handleClick = this._handleClick.bind(this);
+    //this.handleOutletOrder = this.handleOutletOrder.bind(this);
   }
 
-  handleOutletOrder = (items) => {
-    this.setState({items})
-  };
+  async componentDidMount() {
+    console.log(this.context[0]);
+    let url =  window.location.href.includes('localhost')
+      ? 'http://localhost:8888/user/playlists'
+      : 'https://djify-backend.herokuapp.com/user/playlists';
+    let response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+      },
+      params: JSON.stringify({
+        access_token: "BQBIioVKi2tbwFcqh0PkUL8k60CDP1eacC0IwQ4zBHWU562h6ngvHumQLcsMlJFB4H5oZXRcz-xLzR6d9kMvfgjdV6nn8m3TS8UOUVwCH-pRG5LZTF-2kNcp0t1VTvToSQI00sM2sGj-AP2FWojYLFqe4G9l3PRdJdqajWc_fUKNAsLDZEHzIdNqZ0H5yUVWmOlJowkgkVf9PPVKhuTS&spotify_user_id=7229nfdot10lcxq028prmid1j"
+      })
+    });
+    let responseJson = await response.json();
+  }
+
+  // handleOutletOrder = (items) => {
+  //   this.setState({items})
+  // };
+
+  _handleClick(selected) {
+    this.setState({selected, changedValues: true});
+  }
 
   render() {
     return(
       <div id="room">
-        <button
-            className="float-btn link-btn">
-          <Link to="/dashboard">
-            Back
-          </Link>
-        </button>
-        <PartyAnimation user={fakeUser}/>
-        <div id="room-controls" className="center">
-          {
-            this.state.isDj ?
-              <div>
-                <br />
-                <div className="center">
-                  {
-                    this.state.amountAhead === 0 ?
-                      <div>
-                        <Progress percent={Math.abs(this.state.percent)} status={this.state.percent >= 0 ? "success" : "error"} />
-                        {
-                          this.state.percent >= 0 ? "You are on your way to an encore!" : "Too many dislikes, you might get booted!"
-                        }
-                      </div>
-                      :
-                      <div className="amount-label">
-                        {this.state.amountAhead} DJs ahead of you.
-                      </div>
-                  }
-                </div>
-                <br />
-                <hr />
-              </div>
-                :
-              null
-          }
-          <SongDisplay
-            song={fakeSong}
-            isDj={this.state.isDj}/>
-          {
-            this.state.isDj ?
-              <QueueDisplay
-                handleOrder={this.handleOutletOrder}
-                songs={this.state.items}/>
-              :
-              <JudgeDisplay
-                dj={fakeUser}/>
-          }
+      <button
+    className="float-btn link-btn">
+      <Link to="/dashboard">
+      Back
+      </Link>
+      </button>
+      <PartyAnimation user={fakeUser}/>
+    <div id="room-controls" className="center">
+      {/*{*/}
+    {/*  this.state.isDj ?*/}
+    {/*    <div>*/}
+    {/*      <br />*/}
+    {/*      <div className="center">*/}
+    {/*        {*/}
+    {/*          this.state.amountAhead === 0 ?*/}
+    {/*            <div>*/}
+    {/*              <Progress percent={Math.abs(this.state.percent)} status={this.state.percent >= 0 ? "success" : "error"} />*/}
+    {/*              {*/}
+    {/*                this.state.percent >= 0 ? "You are on your way to an encore!" : "Too many dislikes, you might get booted!"*/}
+    {/*              }*/}
+    {/*            </div>*/}
+    {/*            :*/}
+    {/*            <div className="amount-label">*/}
+    {/*              {this.state.amountAhead} DJs ahead of you.*/}
+    {/*            </div>*/}
+    {/*        }*/}
+    {/*      </div>*/}
+    {/*      <br />*/}
+    {/*      <hr />*/}
+    {/*    </div>*/}
+    {/*      :*/}
+    {/*    null*/}
+    {/*}*/}
+  <SongDisplay
+    song={fakeSong}
+    isDj={this.state.isDj}/>
+    <br />
+    <small
+    style={{ marginBottom: 12 }}
+    className="color-neutral">{
+      this.state.changedValues ?
+        "Your changes will happen after this song"
+        : "Select a playlist"
+    }</small>
+    {
+      this.state.isDj && this.state.items.length > 0 &&
+      this.state.items.map((item, index) =>
+          <div
+        onClick={() => this._handleClick(index)}
+      style={{ color: this.state.selected === index ? '#7c89ff' : '#000000' }}
+      className="search-result">
+        <b>{item.name}</b>
         </div>
-      </div>
     )
+    }
+    {/*{*/}
+    {/*  this.state.isDj ?*/}
+    {/*    <QueueDisplay*/}
+    {/*      handleOrder={this.handleOutletOrder}*/}
+    {/*      songs={this.state.items}/>*/}
+    {/*    :*/}
+    {/*    <JudgeDisplay*/}
+    {/*      dj={fakeUser}/>*/}
+    {/*}*/}
+  </div>
+    </div>
+  )
   }
 }
 
